3c9e97e99b5e9f87818d967fc38bf24cb19fb121
Henrik N <henrik@nyh.se>
2011-09-15 21:15:56 +0200

    Add 'hoverable' trigger: hovered tooltip stays put.

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -25,6 +25,16 @@
                 $tip.find('.tipsy-inner')[this.options.html ? 'html' : 'text'](title);
                 $tip[0].className = 'tipsy'; // reset classname in case of dynamic gravity
                 $tip.remove().css({top: 0, left: 0, visibility: 'hidden', display: 'block'}).prependTo(document.body);
+
+                $tip.hover(tipOver, tipOut);
+
+                var that = this;
+                function tipOver() {
+                    that.hoverState = 'in';
+                }
+                function tipOut() {
+                    that.$element.trigger("mouseleave.tipsy");
+                }
                 
                 var pos = $.extend({}, this.$element.offset(), {
                     width: this.$element[0].offsetWidth,
@@ -132,6 +142,9 @@
         }
         
         options = $.extend({}, $.fn.tipsy.defaults, options);
+        if (options.trigger == 'hoverable') {
+            options.delayOut = options.delayOut || 200;
+        }
         
         function get(ele) {
             var tipsy = $.data(ele, 'tipsy');
@@ -167,8 +180,9 @@
         
         if (options.trigger != 'manual') {
             var binder   = options.live ? 'live' : 'bind',
-                eventIn  = options.trigger == 'hover' ? 'mouseenter' : 'focus',
-                eventOut = options.trigger == 'hover' ? 'mouseleave' : 'blur';
+                hoverish = (options.trigger == 'hover' || options.trigger == 'hoverable'),
+                eventIn  = hoverish ? 'mouseenter.tipsy' : 'focus.tipsy',
+                eventOut = hoverish ? 'mouseleave.tipsy' : 'blur.tipsy';
             this[binder](eventIn, enter)[binder](eventOut, leave);
         }
         
