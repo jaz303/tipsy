8aa69ba4f8999626c3a6939f09229bacf0b399a8
Adrien Gibrat <adrien.gibrat@gmail.com>
2012-06-26 21:57:32 +0200

    Fix tipsy-pointee &  allow tooltip re-positionning

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -27,51 +27,53 @@
         show: function() {
             var title = this.getTitle();
             if (title && this.enabled) {
-                var $tip = this.tip();
+                var self = this,
+                    $tip = this.tip();
                 
                 $tip.find('.tipsy-inner')[this.options.html ? 'html' : 'text'](title);
                 $tip[0].className = 'tipsy'; // reset classname in case of dynamic gravity
-                $tip.remove().css({top: 0, left: 0, visibility: 'hidden', display: 'block'}).prependTo(document.body);
-                
-                var pos = $.extend({}, this.$element.offset(), {
-                    width: this.$element[0].offsetWidth,
-                    height: this.$element[0].offsetHeight
-                });
-                
-                var actualWidth = $tip[0].offsetWidth,
-                    actualHeight = $tip[0].offsetHeight,
-                    gravity = maybeCall(this.options.gravity, this.$element[0]);
-                
-                var tp;
-                switch (gravity.charAt(0)) {
-                    case 'n':
-                        tp = {top: pos.top + pos.height + this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2};
-                        break;
-                    case 's':
-                        tp = {top: pos.top - actualHeight - this.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2};
-                        break;
-                    case 'e':
-                        tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth - this.options.offset};
-                        break;
-                    case 'w':
-                        tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width + this.options.offset};
-                        break;
-                }
-                
-                if (gravity.length == 2) {
-                    if (gravity.charAt(1) == 'w') {
-                        tp.left = pos.left + pos.width / 2 - 15;
-                    } else {
-                        tp.left = pos.left + pos.width / 2 - actualWidth + 15;
-                    }
-                }
-                
-                $tip.css(tp).addClass('tipsy-' + gravity);
-                $tip.find('.tipsy-arrow')[0].className = 'tipsy-arrow tipsy-arrow-' + gravity.charAt(0);
+                $tip.remove()
+                .css({top: 0, left: 0, visibility: 'hidden', display: 'block'})
+                .prependTo(document.body)
+				.data('tipsy-pointee', this.$element[0]);
+				var actualWidth = $tip[0].offsetWidth,
+					actualHeight = $tip[0].offsetHeight,
+					gravity = maybeCall(this.options.gravity, this.$element[0]);
+				$tip.bind( 'position', function () {
+					var pos = $.extend({}, self.$element.offset(), {
+						width: self.$element[0].offsetWidth,
+						height: self.$element[0].offsetHeight
+					});
+					var tp;
+					switch (gravity.charAt(0)) {
+						case 'n':
+							tp = {top: pos.top + pos.height + self.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2};
+							break;
+						case 's':
+							tp = {top: pos.top - actualHeight - self.options.offset, left: pos.left + pos.width / 2 - actualWidth / 2};
+							break;
+						case 'e':
+							tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth - self.options.offset};
+							break;
+						case 'w':
+							tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width + self.options.offset};
+							break;
+					}
+					if (gravity.length == 2) {
+						if (gravity.charAt(1) == 'w') {
+							tp.left = pos.left + pos.width / 2 - 15;
+						} else {
+							tp.left = pos.left + pos.width / 2 - actualWidth + 15;
+						}
+					}
+					$tip.css(tp);
+                })
+                .trigger('position')
+                .addClass('tipsy-' + gravity)
+                .find('.tipsy-arrow')[0].className = 'tipsy-arrow tipsy-arrow-' + gravity.charAt(0);
                 if (this.options.className) {
                     $tip.addClass(maybeCall(this.options.className, this.$element[0]));
                 }
-                
                 if (this.options.fade) {
                     $tip.stop().css({opacity: 0, display: 'block', visibility: 'visible'}).animate({opacity: this.options.opacity});
                 } else {
@@ -111,7 +113,6 @@
         tip: function() {
             if (!this.$tip) {
                 this.$tip = $('<div class="tipsy"></div>').html('<div class="tipsy-arrow"></div><div class="tipsy-inner"></div>');
-                this.$tip.data('tipsy-pointee', this.$element[0]);
             }
             return this.$tip;
         },
