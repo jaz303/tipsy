f46a127f5a0c8e86265c4c762089d89472dd2ba5
Ben Buchanan <ben.buchanan@atlassian.com>
2012-09-24 12:28:14 +1000

    Made ARIA optional and attribute removed during hide as it is invalid once the tooltip is gone

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -84,9 +84,11 @@
                     $tip.css({visibility: 'visible', opacity: this.options.opacity});
                 }
 
-                var $tipID = tipsyID();
-                $tip.attr("id", $tipID);
-                this.$element.attr("aria-describedby", $tipID);
+                if (this.options.aria) {
+                    var $tipID = tipsyID();
+                    $tip.attr("id", $tipID);
+                    this.$element.attr("aria-describedby", $tipID);
+                }
             }
         },
         
@@ -95,6 +97,9 @@
                 this.tip().stop().fadeOut(function() { $(this).remove(); });
             } else {
                 this.tip().remove();
+            }
+            if (this.options.aria) {
+                this.$element.removeAttr("aria-describedby");
             }
         },
         
@@ -195,6 +200,7 @@
     };
     
     $.fn.tipsy.defaults = {
+        aria: false,
         className: null,
         delayIn: 0,
         delayOut: 0,
@@ -250,19 +256,19 @@
      *        component.
      */
      $.fn.tipsy.autoBounds = function(margin, prefer) {
-		return function() {
-			var dir = {ns: prefer[0], ew: (prefer.length > 1 ? prefer[1] : false)},
-			    boundTop = $(document).scrollTop() + margin,
-			    boundLeft = $(document).scrollLeft() + margin,
-			    $this = $(this);
+        return function() {
+            var dir = {ns: prefer[0], ew: (prefer.length > 1 ? prefer[1] : false)},
+                boundTop = $(document).scrollTop() + margin,
+                boundLeft = $(document).scrollLeft() + margin,
+                $this = $(this);
 
-			if ($this.offset().top < boundTop) dir.ns = 'n';
-			if ($this.offset().left < boundLeft) dir.ew = 'w';
-			if ($(window).width() + $(document).scrollLeft() - $this.offset().left < margin) dir.ew = 'e';
-			if ($(window).height() + $(document).scrollTop() - $this.offset().top < margin) dir.ns = 's';
+            if ($this.offset().top < boundTop) dir.ns = 'n';
+            if ($this.offset().left < boundLeft) dir.ew = 'w';
+            if ($(window).width() + $(document).scrollLeft() - $this.offset().left < margin) dir.ew = 'e';
+            if ($(window).height() + $(document).scrollTop() - $this.offset().top < margin) dir.ns = 's';
 
-			return dir.ns + (dir.ew ? dir.ew : '');
-		}
-	};
+            return dir.ns + (dir.ew ? dir.ew : '');
+        }
+    };
     
 })(jQuery);
