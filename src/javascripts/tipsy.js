c99c2977808106bf0e83bf87fa2111130bbd1c7a
Jason Frame <jason@onehackoranother.com>
2013-06-15 11:04:06 +0100

    positioning code

--- src/javascripts/tipsy.js
+++ src/javascripts/tipsy.js
@@ -41,6 +41,49 @@ function Tipsy(pointee, options) {
 }
 
 Tipsy.prototype = {
+  reposition: function() {
+    var pm = $.extend({}, this.pointee.offset(), {
+      width   : this.pointee[0].offsetWidth,
+      height  : this.pointee[0].offsetHeiight
+    });
+    
+    var tm = {
+      width   : this.tip.ele[0].offsetWidth,
+      height  : this.tip.ele[0].offsetHeight
+    };
+    
+    var gravity = value(this.options.gravity, this.pointee[0]),
+        top     = null,
+        left    = null;
+        
+    switch (gravity[0]) {
+      case 'n':
+        top = pm.top + pm.height;
+        left = pm.left + pm.width / 2 - tm.width / 2;
+        break;
+      case 's':
+        top = pm.top - tm.height;
+        left = pm.left + pm.width / 2 - tm.width / 2;
+        break;
+      case 'e':
+        top = pm.top + pm.height / 2 - tm.height / 2;
+        left = pm.left - tm.width;
+        break;
+      case 'w':
+        top = pm.top + pm.height / 2 - tm.height / 2;
+        left = pm.left + pm.width;
+        break;
+    }
+        
+    if (gravity[1] === 'w') {
+      left = pm.left + pm.width / 2 - 15;
+    } else if (gravity[1] === 'e') {
+      left = pm.left + pm.width / 2 - tm.width + 15;
+    }
+    
+    this.tip.ele.css({top: top, left: left});
+  },
+  
   show: function(callback) {
     
   },
