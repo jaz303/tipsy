13fb7fb6dc5a966c2ff0f73d635ae51dcb501671
Bogdan Gradinariu <bogdan.gradinariu@xivic.com>
2013-10-03 12:03:24 +0300

    updated "live" method to be compatible with the newer jQuery versions (in which the live method is deprecated)

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -174,11 +174,15 @@
         if (!options.live) this.each(function() { get(this); });
         
         if (options.trigger != 'manual') {
-            var binder   = options.live ? 'live' : 'bind',
-                eventIn  = options.trigger == 'hover' ? 'mouseenter' : 'focus',
+            var eventIn  = options.trigger == 'hover' ? 'mouseenter' : 'focus',
                 eventOut = options.trigger == 'hover' ? 'mouseleave' : 'blur';
-            this[binder](eventIn, enter)[binder](eventOut, leave);
+            if(options.live){
+              $(this.context).on(eventIn, this.selector, enter).on(eventOut, this.selector, leave);
+            } else {
+              this.on(eventIn, enter).on(eventOut, leave);
+            }
         }
+            
         
         return this;
         
